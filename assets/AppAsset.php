4580f24aea9a8c9089fbb26ee434d15e48c1353b
Charlie Root <root@cron.mbox.tw>
2019-12-20 01:52:11 +0800

    add materialize css

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -21,8 +21,10 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
+        'css/materialize.min.css',
     ];
     public $js = [
+        'js/materialize.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
